d9666eeb5850c82dd8a1913517b8ba2caa063461
Pravin <pravinkumar2108@gmail.com>
2023-04-15 02:28:43 +0530

    imported django User to Customusers

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 from . import models
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+
+    """Custom User Admin"""
