9d97ea2c106c97b5d7cb731180c5802947eea91a
Pravin <pravinkumar2108@gmail.com>
2023-04-29 04:11:00 +0530

    Created Rooms and RoomsTypes

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,8 +1,13 @@
+# Register your models here.
 from django.contrib import admin
 from . import models
 
 
-# Register your models here.
+@admin.register(models.RoomType)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
     pass
