4de9c8fac998603914f65d4d36a9f2cb0d1b2d27
Pravin <pravinkumar2108@gmail.com>
2023-04-30 02:27:21 +0530

    Create Photo Feilds Inside Room Panel

--- rooms/admin.py
+++ rooms/admin.py
@@ -5,9 +5,20 @@ from . import models
 
 @admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
+    """Item Admin Model"""
+
     pass
 
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+    """Room Admin Definition"""
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """ """
+
     pass
--- rooms/models.py
+++ rooms/models.py
@@ -19,6 +19,10 @@ class AbstractItem(core_models.TimeStampedModel):
 class RoomType(AbstractItem):
     """RoomType Object Definition"""
 
+    class Meta:
+        verbose_name = "Room Type"
+        ordering = ["name"]
+
     pass
 
     def __str__(self):
@@ -28,17 +32,37 @@ class RoomType(AbstractItem):
 class Amenity(AbstractItem):
     """Amenity Object Definition"""
 
+    class Meta:
+        verbose_name_plural = "Amenities"
+
     pass
 
 
 class Facility(AbstractItem):
     """Facility Model Definition"""
 
+    class Meta:
+        verbose_name_plural = "Facilities"
+
     pass
 
 
 class HouseRule(AbstractItem):
+    class Meta:
+        verbose_name = "House Rule"
+
     pass
+
+
+class Photo(core_models.TimeStampedModel):
+    """Photo Model Definition"""
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampedModel):
@@ -58,11 +82,11 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField(Amenity)
-    facilities = models.ManyToManyField(Facility)
-    house_rules = models.ManyToManyField(HouseRule)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
