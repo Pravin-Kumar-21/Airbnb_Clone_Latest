0f8c9026d9cbcebc92fdb0b762dbf108699c544d
Pravin <pravinkumar2108@gmail.com>
2023-05-02 14:33:06 +0530

    Created App Conversations and added message and user Feilds

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ PROJECT_APPS = [
     "reviews.apps.ReviewsConfig",
     "reservations.apps.ReservationsConfig",
     "lists.apps.ListsConfig",
+    "conversations.apps.ConversationsConfig",
 ]
 THIRD_PARTY_APPS = ["django_countries" "reviews.apps."]
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
