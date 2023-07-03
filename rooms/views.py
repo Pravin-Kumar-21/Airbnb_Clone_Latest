c7bbc86c4cf777aa7c6a289c203f452f30125c6e
Pravin <pravinkumar2108@gmail.com>
2023-07-03 23:45:02 +0530

    extended django templates to create header and footer

--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime
+from . import models
 
 
 def all_rooms(request):
-    now = datetime.now()
-    hungry = True
-    return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
