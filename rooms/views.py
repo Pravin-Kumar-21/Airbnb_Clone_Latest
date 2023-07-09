957013eb5d6b8f4da0921755b15ee094121b2ac2
Pravin <pravinkumar2108@gmail.com>
2023-07-09 14:43:32 +0530

    Created a function that give next and previous page by using django tag

--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from . import models
+import math
 
 
 def all_rooms(request):
-    page = int(request.GET.get("page", 1))
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
     page_size = 10
     limit = page_size * page
     offset = limit - page_size
+    page_count = models.Room.objects.count()
+    page_count = math.ceil(page_count / page_size)
     all_rooms = models.Room.objects.all()[offset:limit]
-    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+    return render(
+        request,
+        "rooms/home.html",
+        {
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count + 1),
+        },
+    )
