e3b9cc2c0a2d2e794dd6c3baf390e86cf7e5e017
Pravin <pravinkumar2108@gmail.com>
2023-07-11 11:18:06 +0530

    Added django Paginator in rooms

--- rooms/views.py
+++ rooms/views.py
@@ -1,24 +1,18 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
-import math
 
 
 def all_rooms(request):
     page = request.GET.get("page", 1)
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    page_count = models.Room.objects.count()
-    page_count = math.ceil(page_count / page_size)
-    all_rooms = models.Room.objects.all()[offset:limit]
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
+    print(vars(rooms))
     return render(
         request,
         "rooms/home.html",
         {
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count + 1),
+            "rooms": rooms,
         },
     )
