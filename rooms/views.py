e0bcf8dbe6ef156dffc2f38e565f177d009964da
Pravin <pravinkumar2108@gmail.com>
2023-07-11 13:07:03 +0530

    created Exception handling in url

--- rooms/views.py
+++ rooms/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
 def all_rooms(request):
     page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    print(vars(rooms))
-    return render(
-        request,
-        "rooms/home.html",
-        {
-            "rooms": rooms,
-        },
-    )
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.page(int(page))
+        return render(
+            request,
+            "rooms/home.html",
+            {
+                "page": rooms,
+            },
+        )
+    except EmptyPage:
+        return redirect("/")
