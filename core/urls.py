8f75117522d74c3e5f3a08c45818be8c914d7854
Pravin <pravinkumar2108@gmail.com>
2023-07-11 19:40:05 +0530

    Created room pages next and prev using a class in views.py

--- core/urls.py
+++ core/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 from rooms import views as room_views
 
 app_name = "core"
-urlpatterns = [path("", room_views.all_rooms, name="home")]
+urlpatterns = [path("", room_views.HomeView.as_view(), name="home")]
