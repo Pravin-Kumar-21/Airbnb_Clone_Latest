1d377ee8a911a298238af2c8c00dc08fe6d847b8
Pravin <pravinkumar2108@gmail.com>
2023-07-19 20:14:05 +0530

    started with login forms

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,7 @@ Including another URLconf
 urlpatterns = [
     path("", include("core.urls", namespace="core")),
     path("rooms/", include("rooms.urls", namespace="rooms")),
+    path("users/", include("users.urls", namespace="users")),
     path("admin/", admin.site.urls),
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "users/login.html")
+
+    def post(self, request):
+        pass
