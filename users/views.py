f979ff2b31a51d81efe890337d196b7f7d119f1a
Pravin <pravinkumar2108@gmail.com>
2023-07-28 23:43:39 +0530

    Github access tokens

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from django.views import View
 from . import forms, models
 from django.contrib.auth import authenticate, login, logout
 import os
+import requests
 
 
 class LoginView(FormView):
@@ -67,4 +68,14 @@ def github_login(request):
 
 
 def github_callback(request):
-    pass
+    client_id = os.environ.get("GH_ID")
+    client_secret = os.environ.get("GH_SECRET")
+    code = request.GET.get("code", None)
+    if code is not None:
+        request = requests.post(
+            f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={{code}}",
+            headers={"Accept": "application/json"},
+        )
+        print(request.json())
+    else:
+        return redirect(reverse("core:home"))
