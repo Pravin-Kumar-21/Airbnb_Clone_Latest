2424508a21e588b6edc3eb9b01ad98c4a2036746
Pravin <pravinkumar2108@gmail.com>
2023-08-05 04:03:05 +0530

    Finally Frontend Working(Tailwind-scss)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,28 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import postcss from 'rollup-plugin-postcss'; // Add the missing import
+import tailwindcss from 'tailwindcss';
+import path from 'path';
 import vue from '@vitejs/plugin-vue';
+import sassPlugin from 'vite-plugin-sass';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    postcss({
+      plugins: [tailwindcss('./tailwind.config.js')],
+      extract: path.resolve('static/css/style.css'),
+    }),
+    vue(),
+    sassPlugin()
+  ],
+  build: {
+    outDir: 'static',
+  },
+  resolve:{
+    alias:{
+      '/@':path.resolve(__dirname,'assets/scss')
+    }
+  },
   server: {
     proxy: {
       '/': 'http://127.0.0.1:8000', // Adjust the port if your Django server runs on a different port
@@ -12,7 +31,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import 'assets/scss/styles.scss;`,
+        additionalData: `@import '/@/styles.scss';`, // Use the alias to fix the path
       },
     },
   },
