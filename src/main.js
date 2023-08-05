b2bad952359ed020ed44692b32b0758ee265e4ef
Pravin <pravinkumar2108@gmail.com>
2023-08-06 01:30:21 +0530

    started with the nav bar

--- src/main.js
+++
@@ -1,3 +0,0 @@
-// src/main.js
-import 'assets/scss/styles.scss';
-// Your other JS code goes below
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 // tailwind.config.js
 module.exports = {
+  content: ['./templates/**/*.html'],
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
