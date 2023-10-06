996281bdb5f9b573c56b06f734256b766e863bfe
Pravin <pravinkumar2108@gmail.com>
2023-10-06 18:50:20 +0530

    Small Bug fixes when user is logged in or not

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from rooms import views as room_views
+from users import views as user_views
 
 app_name = "core"
-urlpatterns = [path("", room_views.HomeView.as_view(), name="home")]
+urlpatterns = [
+    path("", room_views.HomeView.as_view(), name="home"),
+    path("login/", user_views.LoginView.as_view(), name="vialogin"),
+]
--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-    path("login", views.LoginView.as_view(), name="login"),
+    path("login/", views.LoginView.as_view(), name="login"),
     path("signup/", views.SignUpView.as_view(), name="signup"),
     path("login/github/", views.github_login, name="github-login"),
     path("login/github/callback/", views.github_callback, name="github-callback"),
