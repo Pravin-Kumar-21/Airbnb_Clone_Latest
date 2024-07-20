65ce2f0d6d7bf739c1e5bc4ef93ba55078d51190
Pravin <pravinkumar2108@gmail.com>
2024-07-21 01:46:28 +0530

    vercel 1.1

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-hit2(fe#jaekomurw^hy0obvua0!v7xc99n#br1g8l^)hmvsb7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".vercel.app", "now.sh", "127.0.0.1", "localhost"]
 
